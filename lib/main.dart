fab2cd7e4df366fe40021cc08018adc0e16c5a98
Ibraheemahmed5 <92911409+ibraheemahmed48@users.noreply.github.com>
2022-12-05 22:30:33 +0300

    Update2

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:notificationsystem/pages/FirstPage/first_main_page.dart';
+import 'package:notificationsystem/pages/Send_news/Send_news_main_page.dart';
 import 'package:notificationsystem/pages/Signin_page/Signin_main_page.dart';
 import 'package:notificationsystem/test.dart';
-
+import 'package:notificationsystem/test2.dart';
 import 'help/Colors.dart';
 import 'help/text_style.dart';
-
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
@@ -17,11 +16,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Notification System',
-      home: First_main_page(),
-      //home: test()
-     //home: Signin_main_page(),
-      //هاي يلسلسبيل
-
+      //home: First_main_page(),
+      home: Send_news_main_page(),
+    //home: Signin_main_page(),
+       //home: test2(),
     );
   }
 }
