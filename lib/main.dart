ac69611d1892aea54e544f3742af19acd08a04cf
Ibraheemahmed5 <92911409+ibraheemahmed48@users.noreply.github.com>
2022-12-29 00:45:11 +0300

    Edit pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:notificationsystem/View/pages/controler/Routes.dart';
+import 'package:notificationsystem/controler/Routes.dart';
 
 import 'package:notificationsystem/test.dart';
 import 'package:notificationsystem/test2.dart';
