4ee001aeb0d7eaf0dc8e28c9fc88f904a5d4ba99
Ibraheemahmed5 <92911409+ibraheemahmed48@users.noreply.github.com>
2023-02-17 02:32:04 +0300

    Authenticate with Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,60 @@
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:notificationsystem/controler/Routes.dart';
 
-import 'package:notificationsystem/test.dart';
-import 'package:notificationsystem/test2.dart';
-import 'View/pages/Add_student/Add_student_main_page.dart';
-import 'View/pages/FirstPage/first_main_page.dart';
-import 'View/pages/Send_Schedules/Send_Schedules_main_page.dart';
-import 'View/pages/Send_news/Send_news_main_page.dart';
-import 'View/pages/Signin_page/Signin_main_page.dart';
-import 'View/pages/add_stages/Add_stages.dart';
-import 'help/Colors.dart';
-import 'help/text_style.dart';
 
-void main() {
-  runApp(GetMaterialApp.router(
+bool islogin=false;
+void main()  async{
 
-      debugShowCheckedModeBanner: false,
-      title: 'Notification System',
-      //home: First_main_page(),
-      //home: Send_news_main_page(),
-      //home: test2(),
-      //home: Schedules_main_page(),
-      //home: Add_Stages_main_page(),
-      defaultTransition: Transition.fadeIn,
-      getPages: AppPages.pages,
-      routerDelegate: AppRouterDelegate()));
-  ;
+  WidgetsFlutterBinding.ensureInitialized();
+   await Firebase.initializeApp(
+    options: const FirebaseOptions(  apiKey: "AIzaSyAeFlMohXaNLIQ6Gv8JlFpI-8co2e0x7iA",
+        authDomain: "notification-system-ac422.firebaseapp.com",
+        projectId: "notification-system-ac422",
+        storageBucket: "notification-system-ac422.appspot.com",
+        messagingSenderId: "1022720436043",
+        appId: "1:1022720436043:web:0a66294d42cbe467752ace",
+        measurementId: "G-QMH0QSWJE2")
+  );
+   var user = FirebaseAuth.instance.currentUser;
+   if(user == null){
+     islogin=false;
+     print("Email is not LOGIN");
+
+
+   }else{
+     islogin = true;
+     print("Email is  LOGIN");
+
+   }
+
+
+  runApp(const MyApp());
+
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp.router(
+
+        debugShowCheckedModeBanner: false,
+        title: 'Notification System',
+        //home: First_main_page(),
+        //home: Send_news_main_page(),
+        //home: test2(),
+        //home: Schedules_main_page(),
+        //home: Add_Stages_main_page(),
+        defaultTransition: Transition.fadeIn,
+        getPages: AppPages.pages,
+        routerDelegate: AppRouterDelegate());
+  }
 }
 
 /////////////////////////////////////////
